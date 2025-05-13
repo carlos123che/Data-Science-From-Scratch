d033f5a4db944e395ce90e65dcc7df1bbc03b0a5
carlos123che <car123che@gmail.com>
2025-05-13 16:33:00 -0600

    Visualizing Data

---
+++ Visualizing Data/barcharts.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+
+movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+# plot bars with left x-coordinates [0, 1, 2, 3, 4], heights [num_oscars]
+plt.bar(range(len(movies)), num_oscars)
+
+plt.title("My favorite movies")
+plt.ylabel("# of awards")
+
+# label x-axis with movie names at bar centers
+plt.xticks(range(len(movies)), movies)
+plt.show()
